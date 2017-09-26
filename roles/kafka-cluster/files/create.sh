5b43dd9a9ed8c42951cf8bf4f0d0313a41695dc1
siyi.zheng <siyi.zheng@aalto.fi>
2017-09-26 22:22:33 +0300

    check readiness

--- roles/kafka-cluster/files/create.sh
+++ roles/kafka-cluster/files/create.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
+# set -x
+
+check_readiness(){
+while true
+do
+    DESIRED=$(kubectl -n kafka get statefulsets/$1|tail -1|awk '{print $2}')
+    CURRENT=$(kubectl -n kafka get statefulsets/$1|tail -1|awk '{print $3}')
+
+    if [[ $DESIRED -ne $CURRENT ]]; then
+        echo "desired:$DESIRED, current:$CURRENT"
+        echo "sleep 10 sec"
+        sleep 10
+        echo -e "\e[0K\r"
+        echo -en "\e[3A"
+        echo -en "\e[0K\r"
+    else
+        break
+    fi
+done
+}
 
 kubectl apply -f zookeeper/
 
+check_readiness zoo 
+
 kubectl apply -f kafka/
+
+check_readiness kafka
+
+kubectl -n kafka exec kafka-0 -- bin/kafka-topics.sh --create --zookeeper zookeeper.kafka --replication-factor 1 --partitions 1 --topic mqtt-test
 
 kubectl apply -f mqtt/
 kubectl apply -f mongo/
 
 kubectl apply -f connector/mongodb/
 kubectl apply -f connector/mqtt/
+
+# kubectl -n kafka exec kafka-0 -- bin/kafka-topics.sh --list --zookeeper zookeeper.kafka
